1092f9418a35b99fb33a47d7ec59f647c9569158
Blank <wangfj11@foxmail.com>
2024-09-21 14:18:10 +0800

    [增加]1. 增加错误和警告的日志打印

--- Editor/LogHelper.cs
+++ Editor/LogHelper.cs
@@ -4,9 +4,24 @@ namespace GameFrameX.Xcode.Editor
 {
     internal static class LogHelper
     {
+        private static string GetTime()
+        {
+            return $"[GameFrameX-Xcode:{DateTime.Now:HH:mm:ss.fff}]:";
+        }
+
         public static void Log(string msg)
         {
-            UnityEngine.Debug.Log($"[GameFrameX-Xcode:{DateTime.Now:HH:mm:ss.fff}]:" + msg);
+            UnityEngine.Debug.Log(GetTime() + msg);
+        }
+
+        public static void Warning(string msg)
+        {
+            UnityEngine.Debug.LogWarning(GetTime() + msg);
+        }
+
+        public static void Error(string msg)
+        {
+            UnityEngine.Debug.LogError(GetTime() + msg);
         }
     }
 }
