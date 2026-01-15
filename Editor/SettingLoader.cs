e1c93e2c1f9a6131208d229bf71149348c92b57b
Mac <mac@zilancreative.com>
2026-01-15 11:10:09 +0800

    feat(Xcode编辑器): 添加加载匹配配置文件的功能
    新增LoadSettingDatas方法，用于根据文件名部分匹配并加载所有符合条件的JSON配置文件

--- Editor/SettingLoader.cs
+++ Editor/SettingLoader.cs
@@ -1,4 +1,5 @@
 #if UNITY_IOS
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -27,6 +28,34 @@ namespace GameFrameX.Xcode.Editor
 
             File.WriteAllText(fileName, "");
             return null;
+        }
+
+        /// <summary>
+        /// 加载所有匹配的配置文件
+        /// </summary>
+        /// <param name="fileNamePart">文件名包含的部分</param>
+        /// <returns>文件路径列表</returns>
+        public static List<string> LoadSettingDatas(string fileNamePart)
+        {
+            var guids = AssetDatabase.FindAssets($"t:textasset");
+            var results = new List<string>(16);
+
+            foreach (var guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                var newFileName = Path.GetFileName(path);
+
+                // 检查文件名是否包含 fileNamePart (忽略大小写)，且以 .json 结尾
+                if (newFileName.IndexOf(fileNamePart, System.StringComparison.OrdinalIgnoreCase) >= 0 &&
+                    newFileName.EndsWith(".json", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    results.Add(path);
+                }
+            }
+
+            // 排序，保证加载顺序确定
+            results.Sort();
+            return results;
         }
     }
 }
